e63a1cc0b12a678df1b9fc6f0e3398679cd6c136
palvairr <rudy.palvair@xpologistics.com>
2015-12-17 11:02:23 +0100

    Use @SpringBootApplication

--- src/main/java/com.palvair.angularjasminespringboot/HomeController.java
+++ src/main/java/com.palvair.angularjasminespringboot/HomeController.java
@@ -2,6 +2,7 @@ package com.palvair.angularjasminespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by PALVAIRR on 12/10/2015.
  */
 //@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class HomeController {
 
     /*@RequestMapping("/")
